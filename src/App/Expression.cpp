bf7b370e05bc97e1bd3c4efb0754b431efcd17c6
wmayer <wmayer@freecad.org>
2025-03-01 08:31:39 +0100

    App: In ExpressionParser::parse add the failing expression text to the error message

--- src/App/Expression.cpp
+++ src/App/Expression.cpp
@@ -37,6 +37,7 @@
 #include <sstream>
 #include <stack>
 #include <string>
+#include <fmt/format.h>
 
 #include <App/Application.h>
 #include <App/DocumentObject.h>
@@ -3729,14 +3730,17 @@ Expression * App::ExpressionParser::parse(const App::DocumentObject *owner, cons
     // run the parser
     int result = ExpressionParser::ExpressionParser_yyparse ();
 
-    if (result != 0)
-        throw ParserError("Failed to parse expression.");
+    if (result != 0) {
+        throw ParserError(fmt::format("Failed to parse expression '{}'", buffer));
+    }
 
-    if (!ScanResult)
-        throw ParserError("Unknown error in expression");
+    if (!ScanResult) {
+        throw ParserError(fmt::format("Unknown error in expression '{}'", buffer));
+    }
 
-    if (valueExpression)
+    if (valueExpression) {
         return ScanResult;
+    }
     else {
         delete ScanResult;
         throw Expression::Exception("Expression can not evaluate to a value.");
