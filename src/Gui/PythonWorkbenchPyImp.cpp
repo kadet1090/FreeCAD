72c21dc0cfe2da4e7a2157dccd9a50871dbb0b81
Ladislav Michl <ladis@triops.cz>
2025-04-26 20:06:18 +0200

    Gui: cleanup includes in *PyImp.cpp
    Sort includes and implicitely add missing ones. While there
    consistently explain where generated files some from.

--- src/Gui/PythonWorkbenchPyImp.cpp
+++ src/Gui/PythonWorkbenchPyImp.cpp
@@ -25,7 +25,6 @@
 #include "PythonWorkbenchPy.h"
 #include "PythonWorkbenchPy.cpp"
 
-
 using namespace Gui;
 
 /** @class PythonWorkbenchPy
--- src/Gui/WorkbenchPyImp.cpp
+++ src/Gui/WorkbenchPyImp.cpp
@@ -24,7 +24,7 @@
 #include "Workbench.h"
 #include "WorkbenchManager.h"
 
-// inclusion of the generated files (generated out of WorkbenchPy.xml)
+// generated out of Workbench.pyi
 #include "WorkbenchPy.h"
 #include "WorkbenchPy.cpp"
 
