1a2515450b13d7976494381a7e7a24a101adfa3e
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2025-05-12 17:43:25 +0200

    [Mod] Drawing remove (#20419)
    * [Mod] Draw remove
    
    removed deprecated drawing module
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/Tools/updatecrowdin.py
+++ src/Tools/updatecrowdin.py
@@ -115,6 +115,8 @@ locations = [
     ],
     ["App", "../App/Resources/translations", "../App/Resources/App.qrc"],
     ["Arch", "../Mod/BIM/Resources/translations", "../Mod/BIM/Resources/Arch.qrc"],
+    ["App", "../App/Resources/translations", "../App/Resources/App.qrc"],
+    ["Arch", "../Mod/BIM/Resources/translations", "../Mod/BIM/Resources/Arch.qrc"],
     [
         "Assembly",
         "../Mod/Assembly/Gui/Resources/translations",
@@ -126,11 +128,6 @@ locations = [
         "../Mod/Draft/Resources/Draft.qrc",
     ],
     ["Base", "../Base/Resources/translations", "../Base/Resources/Base.qrc"],
-    [
-        "Drawing",
-        "../Mod/Drawing/Gui/Resources/translations",
-        "../Mod/Drawing/Gui/Resources/Drawing.qrc",
-    ],
     [
         "Fem",
         "../Mod/Fem/Gui/Resources/translations",
