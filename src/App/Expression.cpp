32c66f7ef3105fc36d07829a3b53aa4ebed5d320
wmayer <wmayer@freecad.org>
2025-05-16 18:55:43 +0200

    Remove unused DOUBLE_MIN

--- src/App/Expression.cpp
+++ src/App/Expression.cpp
@@ -73,9 +73,6 @@ FC_LOG_LEVEL_INIT("Expression", true, true)
 #ifndef  DOUBLE_MAX
 # define DOUBLE_MAX 1.7976931348623157E+308    /* max decimal value of a "double"*/
 #endif
-#ifndef  DOUBLE_MIN
-# define DOUBLE_MIN 2.2250738585072014E-308    /* min decimal value of a "double"*/
-#endif
 
 #if defined(_MSC_VER)
 #define strtoll _strtoi64
