8ef50ce178d4d846734485429da57387078f4c36
hlorus <64740362+hlorus@users.noreply.github.com>
2025-05-20 12:10:35 +0200

    Measure: Fix delta label text size scaling (#21467)
    * Measure: Fix delta label font size
    
    - Add font size field to ViewProviderMeasureBase which connects to other fields
    - Remove dead code in DimensionLinear which internally is using FrameLabel
    - Connect FrameLabels's fontsize property to DimensionLinear's property
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/Mod/Measure/Gui/ViewProviderMeasureBase.cpp
+++ src/Mod/Measure/Gui/ViewProviderMeasureBase.cpp
@@ -194,6 +194,7 @@ ViewProviderMeasureBase::ViewProviderMeasureBase()
     TextBackgroundColor.touch();
     FontSize.touch();
     LineColor.touch();
+    fieldFontSize.setValue(FontSize.getValue());
 }
 
 ViewProviderMeasureBase::~ViewProviderMeasureBase()
@@ -243,6 +244,7 @@ void ViewProviderMeasureBase::onChanged(const App::Property* prop)
     }
     else if (prop == &FontSize) {
         pLabel->size = FontSize.getValue();
+        fieldFontSize.setValue(FontSize.getValue());
     }
     ViewProviderDocumentObject::onChanged(prop);
 }
