e3fae10a2e7f91042a8ca9b59a336a556c9f2cc1
wwmayer <wmayer@freecad.org>
2025-08-23 13:27:37 +0200

    Mesh: Refactor MeshInput::LoadNastran()

---
+++ src/Mod/Mesh/App/Core/IO/ReaderNAS.h
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+/***************************************************************************
+ *   Copyright (c) 2024 Werner Mayer <wmayer[at]users.sourceforge.net>     *
+ *                                                                         *
+ *   This file is part of FreeCAD.                                         *
+ *                                                                         *
+ *   FreeCAD is free software: you can redistribute it and/or modify it    *
+ *   under the terms of the GNU Lesser General Public License as           *
+ *   published by the Free Software Foundation, either version 2.1 of the  *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   FreeCAD is distributed in the hope that it will be useful, but        *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with FreeCAD. If not, see                               *
+ *   <https://www.gnu.org/licenses/>.                                      *
+ *                                                                         *
+ **************************************************************************/
+
+
+#ifndef MESH_IO_READER_NAS_H
+#define MESH_IO_READER_NAS_H
+
+#include <Mod/Mesh/MeshGlobal.h>
+#include <iosfwd>
+
+namespace Base
+{
+class InputStream;
+}
+
+namespace MeshCore
+{
+
+class MeshKernel;
+
+/** Loads the mesh object from data in NASTRAN format. */
+class MeshExport ReaderNAS
+{
+public:
+    /*!
+     * \brief ReaderSTL
+     */
+    explicit ReaderNAS(MeshKernel& kernel);
+    /*!
+     * \brief Load the mesh from the input stream
+     * \return true on success and false otherwise
+     */
+    bool Load(std::istream& input);
+
+private:
+    MeshKernel& _kernel;
+};
+
+}  // namespace MeshCore
+
+
+#endif  // MESH_IO_READER_NAS_H
