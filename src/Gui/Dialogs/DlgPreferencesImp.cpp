c37407a87ad7c4501897df238b39c6a608d8c679
tetektoza <tetektoza@gmail.com>
2025-09-27 13:33:11 +0200

    Gui: Support searching for tooltips, comboboxes and groups in Prefs
    As the title says. This patch adds support for finding tooltip text,
    also extends widget types to QSpinBoxes/QDoubleSpinBoxes and also adds
    support for finding combobox items from QComboBox dropdown. Also adds
    support for QGroupBox titles.

--- src/Gui/Dialogs/DlgPreferencesImp.cpp
+++ src/Gui/Dialogs/DlgPreferencesImp.cpp
@@ -27,8 +27,10 @@
 #include <QAbstractButton>
 #include <QApplication>
 #include <QCheckBox>
+#include <QComboBox>
 #include <QCursor>
 #include <QDebug>
+#include <QDoubleSpinBox>
 #include <QFrame>
 #include <QGroupBox>
 #include <QLabel>
@@ -41,6 +43,7 @@
 #include <QScreen>
 #include <QScrollArea>
 #include <QScrollBar>
+#include <QSpinBox>
 #include <QTabWidget>
 #include <QTimer>
 #include <QToolButton>
@@ -1480,6 +1483,10 @@ void PreferencesSearchController::collectSearchResults(
     searchWidgetType<QCheckBox>(widget, searchText, groupName, pageName, pageDisplayName, tabName);
     searchWidgetType<QRadioButton>(widget, searchText, groupName, pageName, pageDisplayName, tabName);
     searchWidgetType<QPushButton>(widget, searchText, groupName, pageName, pageDisplayName, tabName);
+    searchWidgetType<QGroupBox>(widget, searchText, groupName, pageName, pageDisplayName, tabName);
+    searchWidgetType<QComboBox>(widget, searchText, groupName, pageName, pageDisplayName, tabName);
+    searchWidgetType<QSpinBox>(widget, searchText, groupName, pageName, pageDisplayName, tabName);
+    searchWidgetType<QDoubleSpinBox>(widget, searchText, groupName, pageName, pageDisplayName, tabName);
 }
 
 void PreferencesSearchController::onSearchResultSelected()
@@ -1650,22 +1657,67 @@ void PreferencesSearchController::searchWidgetType(
         else if constexpr (std::is_same_v<WidgetType, QPushButton>) {
             widgetText = widget->text();
         }
+        else if constexpr (std::is_same_v<WidgetType, QGroupBox>) {
+            widgetText = widget->title();
+        }
 
-        // Use fuzzy matching instead of simple contains
-        int score = 0;
-        if (fuzzyMatch(searchText, widgetText, score)) {
-            SearchResult result {
-                .groupName = groupName,
-                .pageName = pageName,
-                .widget = widget,
-                .matchText = widgetText,
-                .groupBoxName = findGroupBoxForWidget(widget),
-                .tabName = tabName,
-                .pageDisplayName = pageDisplayName,
-                .isPageLevelMatch = false,
-                .score = score
-            };
-            m_searchResults.append(result);
+        if (!widgetText.isEmpty()) {
+            int score = 0;
+            if (fuzzyMatch(searchText, widgetText, score)) {
+                SearchResult result;
+                result.groupName = groupName;
+                result.pageName = pageName;
+                result.widget = widget;
+                result.matchText = widgetText;
+                result.groupBoxName = findGroupBoxForWidget(widget);
+                result.tabName = tabName;
+                result.pageDisplayName = pageDisplayName;
+                result.isPageLevelMatch = false;
+                result.score = score;
+                m_searchResults.append(result);
+            }
+        }
+
+        // search tooltip text for all widget types
+        QString tooltip = widget->toolTip();
+        if (!tooltip.isEmpty()) {
+            int tooltipScore = 0;
+            if (fuzzyMatch(searchText, tooltip, tooltipScore)) {
+                SearchResult result;
+                result.groupName = groupName;
+                result.pageName = pageName;
+                result.widget = widget;
+                result.matchText = QStringLiteral("Tooltip: ") + tooltip;
+                result.groupBoxName = findGroupBoxForWidget(widget);
+                result.tabName = tabName;
+                result.pageDisplayName = pageDisplayName;
+                result.isPageLevelMatch = false;
+                result.score = tooltipScore - 100;  // lower score for tooltip matches
+                m_searchResults.append(result);
+            }
+        }
+
+        // search throughout combobox items
+        if constexpr (std::is_same_v<WidgetType, QComboBox>) {
+            for (int i = 0; i < widget->count(); ++i) {
+                QString itemText = widget->itemText(i);
+                if (!itemText.isEmpty()) {
+                    int itemScore = 0;
+                    if (fuzzyMatch(searchText, itemText, itemScore)) {
+                        SearchResult result;
+                        result.groupName = groupName;
+                        result.pageName = pageName;
+                        result.widget = widget;
+                        result.matchText = QStringLiteral("Option: ") + itemText;
+                        result.groupBoxName = findGroupBoxForWidget(widget);
+                        result.tabName = tabName;
+                        result.pageDisplayName = pageDisplayName;
+                        result.isPageLevelMatch = false;
+                        result.score = itemScore + 50;  // boost score for combo items
+                        m_searchResults.append(result);
+                    }
+                }
+            }
         }
     }
 }
